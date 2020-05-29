366f6ab3a3b5c7ad7a9dc81c4874d08f5d56c064
dyb <dengyb1718@163.com>
2020-05-29 09:54:07 +0800

    增加了Object和数组的转换

---
+++ src/main/java/tools/ObjectAndBytes.java
@@ -0,0 +1,47 @@
+package tools;
+
+import java.io.*;
+
+public class ObjectAndBytes {
+    /**
+     * 对象转数组
+     * @param obj
+     * @return
+     */
+    public byte[] toByteArray (Object obj) {
+        byte[] bytes = null;
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        try {
+            ObjectOutputStream oos = new ObjectOutputStream(bos);
+            oos.writeObject(obj);
+            oos.flush();
+            bytes = bos.toByteArray ();
+            oos.close();
+            bos.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return bytes;
+    }
+
+    /**
+     * 数组转对象
+     * @param bytes
+     * @return
+     */
+    public Object toObject (byte[] bytes) {
+        Object obj = null;
+        try {
+            ByteArrayInputStream bis = new ByteArrayInputStream (bytes);
+            ObjectInputStream ois = new ObjectInputStream (bis);
+            obj = ois.readObject();
+            ois.close();
+            bis.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        }
+        return obj;
+    }
+}
